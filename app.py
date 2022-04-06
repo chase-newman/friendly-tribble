80d5afe231f3dc845cbbfa7851285fcf6e0a9fb4
Chase Newman <chasejn01@gmail.com>
2022-04-06 17:12:48 -0400

    add http client

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 import http.client
+import json
 
 # print("Hello World")
 # print("Hello World")
@@ -20,9 +21,29 @@ with open('dailyibmstock.csv') as csv_file:
     #     line_count += 1
     # print(f'Processed {line_count} lines.')
 
-connection = http.client.HTTPSConnection("https://test-project-d14cc-default-rtdb.firebaseio.com/private.json")
-connection.request("GET", "/")
+
+connection = http.client.HTTPSConnection("test-project-d14cc-default-rtdb.firebaseio.com")
+connection.request("GET", "/private.json")
 response = connection.getresponse()
+print(response.headers)
+data = response.read()
 print("Status: {} and reason: {}".format(response.status, response.reason))
 
+print(data)
+
 connection.close()
+
+
+
+
+conn = http.client.HTTPSConnection('test-project-d14cc-default-rtdb.firebaseio.com')
+
+headers = {'Content-type': 'application/json'}
+
+foo = {'text': 'Hello HTTP #1 **cool**, and #1!'}
+json_data = json.dumps(foo)
+
+conn.request('POST', '/post.json', json_data, headers)
+
+response = conn.getresponse()
+print(response.read().decode())
