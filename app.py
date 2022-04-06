97f3472bbbd0f7f5bbf0e74d661a9770206ac6ba
Chase Newman <chasejn01@gmail.com>
2022-04-06 16:52:33 -0400

    add IBM stock data csv

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import csv
+import http.client
 
 # print("Hello World")
 # print("Hello World")
@@ -10,16 +11,18 @@ import csv
 # city = input("What city do you live in? ")
 # print("Name: " + name + " Age: " + age + " City: " + city)
 
-with open('employees.csv') as csv_file:
+with open('dailyibmstock.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     print(csv_reader)
-    line_count = 0
-    for row in csv_reader:
-        print(row)
-        # if line_count == 0:
-        #     print(f'Column names are {", ".join(row)}')
-        #     line_count += 1
-        # else:
-        #     print("Not Applicable")
-        #     line_count += 1
-    print(f'Processed {line_count} lines.')
+    # line_count = 0
+    # for row in csv_reader:
+    #     print(row)
+    #     line_count += 1
+    # print(f'Processed {line_count} lines.')
+
+connection = http.client.HTTPSConnection("https://test-project-d14cc-default-rtdb.firebaseio.com/private.json")
+connection.request("GET", "/")
+response = connection.getresponse()
+print("Status: {} and reason: {}".format(response.status, response.reason))
+
+connection.close()
